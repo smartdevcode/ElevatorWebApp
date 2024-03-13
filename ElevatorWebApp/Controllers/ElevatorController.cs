fa533fcdd316a13bfae33e6b5f7d0c670f06be08
Senior developer <senior.dev.software@gmail.com>
2024-03-13 10:51:54 -0700

    completed the task

---
+++ ElevatorWebApp/Controllers/ElevatorController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ElevatorWebApp.Models;
+
+namespace ElevatorWebApp.Controllers
+{
+    public class ElevatorController : Controller
+    {
+        private static Elevator elevator = new Elevator();
+
+        public IActionResult Index()
+        {
+            return View(elevator);
+        }
+
+        [HttpPost]
+        public IActionResult RequestElevator(int floor, RequestDirection direction = RequestDirection.None)
+        {
+            elevator.AddFloorRequest(floor, direction);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult MoveElevator()
+        {
+            elevator.MoveToNextRequest();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ElevatorWebApp/Models/Elevator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ElevatorWebApp.Models
+{
+    public enum RequestDirection { Up, Down, None }
+
+    public class FloorRequest
+    {
+        public int Floor { get; set; }
+        public RequestDirection Direction { get; set; }
+    }
+
+    public class Elevator
+    {
+        public int CurrentFloor { get; private set; } = 1;
+        public List<FloorRequest> FloorRequests { get; private set; } = new List<FloorRequest>();
+
+        public void AddFloorRequest(int floor, RequestDirection direction = RequestDirection.None)
+        {
+            if (!FloorRequests.Any(r => r.Floor == floor && r.Direction == direction))
+            {
+                FloorRequests.Add(new FloorRequest { Floor = floor, Direction = direction });
+            }
+        }
+
+        // Simulate moving to the next requested floor
+        // This method can be expanded to consider the direction of requests for more complex logic
+        public void MoveToNextRequest()
+        {
+            if (FloorRequests.Count > 0)
+            {
+                var nextRequest = FloorRequests.First();
+                CurrentFloor = nextRequest.Floor;
+                FloorRequests.RemoveAt(0);
+            }
+        }
+    }
+}
